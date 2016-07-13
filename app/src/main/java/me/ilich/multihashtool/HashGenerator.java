9799cd7ddd6359bef3527f3353f24ec935ed2954
HWer <Onishchuk.aleksey@gmail.com>
2016-07-13 17:04:42 +0400

    Поменял

--- app/src/main/java/me/ilich/multihashtool/HashGenerator.java
+++ app/src/main/java/me/ilich/multihashtool/HashGenerator.java
@@ -1,5 +1,7 @@
 package me.ilich.multihashtool;
 
+import sun.misc.BASE64Encoder;
+
 import java.net.URL;
 
 /**
@@ -7,7 +9,7 @@ import java.net.URL;
  */
 public abstract class HashGenerator {
 
-    public String asPlain(String alias, URL keyStore, String storePass) {
+    public final String asPlain(String alias, URL keyStore, String storePass) {
         if (alias == null){
             throw new NullPointerException("alias");
         }
@@ -17,10 +19,10 @@ public abstract class HashGenerator {
         if (storePass == null){
             throw new NullPointerException("storePass");
         }
-        return onGenerate(alias, keyStore, storePass);
+        return onGenerate(alias, keyStore, storePass).toUpperCase();
     }
 
-    public String asBase64(String alias, URL keyStore, String storePass) {
+    public final String asBase64(String alias, URL keyStore, String storePass) {
         if (alias == null){
             throw new NullPointerException("alias");
         }
@@ -30,7 +32,35 @@ public abstract class HashGenerator {
         if (storePass == null){
             throw new NullPointerException("storePass");
         }
-        return onGenerate(alias, keyStore, storePass);
+        return getBase64FromHEX(onGenerate(alias, keyStore, storePass));
+    }
+
+    private static String getBase64FromHEX(String input) {
+        byte barr[] = new byte[input.length()/2];
+        int bcnt = 0;
+        for (int i = 0; i < input.length(); i += 2) {
+            char c1 = input.charAt(i);
+            char c2 = input.charAt(i + 1);
+            int i1 = getIntFromChar(c1);
+            int i2 = getIntFromChar(c2);
+            barr[bcnt] = 0;
+            barr[bcnt] |= (byte) ((i1 & 0x0F) << 4);
+            barr[bcnt] |= (byte) (i2 & 0x0F);
+            bcnt++;
+        }
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(barr);
+    }
+
+    private static int getIntFromChar(char c) {
+        char[] carr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        char clower = Character.toLowerCase(c);
+        for (int i = 0; i < carr.length; i++) {
+            if (clower == carr[i]) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     protected abstract String onGenerate(String alias, URL keyStore, String storePass);
--- app/src/main/java/me/ilich/multihashtool/Sha1HashGenerator.java
+++ app/src/main/java/me/ilich/multihashtool/Sha1HashGenerator.java
@@ -34,15 +34,12 @@ public class Sha1HashGenerator extends HashGenerator {
             e.printStackTrace();
         }
 
-        //String myPath = keyStore.getPath().substring(1, keyStore.getPath().length());
-        //Path srcDir = FileSystems.getDefault().getPath(myPath);
-
         try {
             tmpDir = Files.createTempDirectory("tmp");
             tmpDebugKeyStore = Files.createTempFile(tmpDir, "tmp_file", ".keystore");
             Files.copy(inStream, tmpDebugKeyStore, StandardCopyOption.REPLACE_EXISTING);
             tmpCert = Files.createTempFile(tmpDir, "crt", ".crt");
-            } catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
@@ -83,63 +80,6 @@ public class Sha1HashGenerator extends HashGenerator {
         }
 
         return shaFromFile;
-    }
-
-    @Override
-    public String asPlain(String alias, URL keyStore, String storePass) {
-        if (alias == null){
-            throw new NullPointerException("alias");
-        }
-        if (keyStore == null){
-            throw new NullPointerException("keyStore");
-        }
-        if (storePass == null){
-            throw new NullPointerException("storePass");
-        }
-        return onGenerate(alias, keyStore, storePass).toUpperCase();
-    }
-    @Override
-    public String asBase64(String alias, URL keyStore, String storePass) {
-        if (alias == null){
-            throw new NullPointerException("alias");
-        }
-        if (keyStore == null){
-            throw new NullPointerException("keyStore");
-        }
-        if (storePass == null){
-            throw new NullPointerException("storePass");
-        }
-        String shaFromFile = onGenerate(alias, keyStore, storePass);
-        return getBase64FromHEX(shaFromFile);
-    }
-
-
-    private static String getBase64FromHEX(String input) {
-        byte barr[] = new byte[input.length()/2];
-        int bcnt = 0;
-        for (int i = 0; i < input.length(); i += 2) {
-            char c1 = input.charAt(i);
-            char c2 = input.charAt(i + 1);
-            int i1 = getIntFromChar(c1);
-            int i2 = getIntFromChar(c2);
-            barr[bcnt] = 0;
-            barr[bcnt] |= (byte) ((i1 & 0x0F) << 4);
-            barr[bcnt] |= (byte) (i2 & 0x0F);
-            bcnt++;
-        }
-        BASE64Encoder encoder = new BASE64Encoder();
-        return encoder.encode(barr);
-    }
-
-    private static int getIntFromChar(char c) {
-        char[] carr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-        char clower = Character.toLowerCase(c);
-        for (int i = 0; i < carr.length; i++) {
-            if (clower == carr[i]) {
-                return i;
-            }
-        }
-        return 0;
     }
 
     private static String getShaFromFile(String f) throws NoSuchAlgorithmException, IOException {
