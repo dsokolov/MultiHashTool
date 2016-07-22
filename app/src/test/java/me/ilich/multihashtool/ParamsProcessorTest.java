6cd89af2d1e3dde70c3e25f956a7bab65f05acf8
HWer <Onishchuk.aleksey@gmail.com>
2016-07-22 13:02:58 +0400

    Изменил тесты

--- app/src/test/java/me/ilich/multihashtool/ParamsProcessorTest.java
+++ app/src/test/java/me/ilich/multihashtool/ParamsProcessorTest.java
@@ -3,9 +3,7 @@ package me.ilich.multihashtool;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  * Created by Aleks on 13.07.2016.
@@ -25,8 +23,8 @@ public class ParamsProcessorTest {
     @Test
     public void Sha1forVk() {
         try {
-            OutputParams[] outputParams = paramsProcessor.process(new InputParams("androiddebugkey", Sha1HashGeneratorTest.class.getResource("/debug.keystore"), "android"));
-            assertEquals("76107487556539B90F5466175B98D18716002CB8", outputParams[0].toString());
+            OutputParams[] outputParams = paramsProcessor.process(InputParams.readFromFile("appconfig.json"));
+            assertEquals("76107487556539B90F5466175B98D18716002CB8", outputParams[0].getSHA1());
         } catch (Exception e) {
             e.printStackTrace();
             fail();
@@ -36,8 +34,8 @@ public class ParamsProcessorTest {
     @Test
     public void Sha1forFb() {
         try {
-            OutputParams[] outputParams = paramsProcessor.process(new InputParams("androiddebugkey", Sha1HashGeneratorTest.class.getResource("/debug.keystore"), "android"));
-            assertEquals("dhB0h1VlObkPVGYXW5jRhxYALLg=", outputParams[1].toString());
+            OutputParams[] outputParams = paramsProcessor.process(InputParams.readFromFile("appconfig.json"));
+            assertEquals("dhB0h1VlObkPVGYXW5jRhxYALLg=", outputParams[1].getSHA1());
         } catch (Exception e) {
             e.printStackTrace();
             fail();
@@ -47,10 +45,10 @@ public class ParamsProcessorTest {
     @Test
     public void createHTML() {
         try {
-            OutputParams[] outputParams = paramsProcessor.process(new InputParams("androiddebugkey", Sha1HashGeneratorTest.class.getResource("/debug.keystore"), "android"));
+            OutputParams[] outputParams = paramsProcessor.process(InputParams.readFromFile("appconfig.json"));
             htmlCreator.generateHTML(outputParams);
             htmlCreator.print("SHA1.html");
-            assertEquals("76107487556539B90F5466175B98D18716002CB8", outputParams[0].toString());
+            assertEquals("76107487556539B90F5466175B98D18716002CB8", outputParams[0].getSHA1());
         } catch (Exception e) {
             e.printStackTrace();
             assertTrue(false);
